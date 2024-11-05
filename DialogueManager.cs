8eca15bed7e07a4c69c6cb733060ebdc9350d237
0248913 <142315920+0248913@users.noreply.github.com>
2024-11-05 11:08:34 +1100

    Create DialogueManager.cs

---
+++ DialogueManager.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using UnityEngine.EventSystems;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+
+    private TextMeshProUGUI[] choicesText;
+
+
+    private Story currentStory;
+
+    public bool DialogueIsPlaying { get; private set; }
+
+
+    private static DialogueManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found more than one Dialogue Manager");
+        }
+        instance = this;
+    }
+
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        DialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+    }
+
+    private void Update()
+    {
+        if (!DialogueIsPlaying)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            ContinueStory();
+        }
+    }
+
+
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+
+        currentStory = new Story(inkJSON.text);
+        DialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private void ExitDialogueMode()
+    {
+        DialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+
+            DisplayChoices();
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        Debug.Log($"Number of choices available: {currentChoices.Count}");
+
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than UI can support: " + currentChoices.Count);
+        }
+
+        int index = 0;
+
+        foreach (Choice choice in currentChoices)
+        {
+            if (index < choices.Length) 
+            {
+                choices[index].gameObject.SetActive(true);
+                choicesText[index].text = choice.text;
+                Debug.Log($"Choice {index}: {choice.text}");
+                index++;
+            }
+        }
+
+       
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+
+
+    private IEnumerator SelectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        Debug.Log("MakeChoice called with index: " + choiceIndex);
+
+        foreach (GameObject choiceButton in choices)
+        {
+            choiceButton.SetActive(false);
+        }
+
+        if (choiceIndex >= 0 && choiceIndex < currentStory.currentChoices.Count)
+        {
+            Debug.Log("Valid choice made, continuing story...");
+            currentStory.ChooseChoiceIndex(choiceIndex);
+            ContinueStory();
+        }
+        else
+        {
+            Debug.LogError("Choice index out of range: " + choiceIndex);
+            Debug.LogError("No choices available to select.");
+        }
+    }
+}
